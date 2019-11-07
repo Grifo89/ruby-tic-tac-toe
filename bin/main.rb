86ca729f8add3503ec36bed017986e4b0cc6d839
Christian Salazar <calaca89@gmail.com>
2019-11-07 17:43:07 -0500

    Adding user interface

---
+++ bin/main.rb
@@ -0,0 +1,68 @@
+
+PLAYER_ONE = 'X'
+PLAYER_TWO = 'O'
+
+def initialize_board
+  @count = 9
+  @player = PLAYER_ONE
+  @board = Array.new(3){ Array.new(3, " ") }
+end
+
+def get_inputs
+  begin
+    print "#{@player}, please enter the row: \n"
+    input_row = gets
+
+    print "#{@player}, please enter the column: \n"
+    input_column = gets
+
+    if(!valid = valid_move?(input_row.to_i, input_column.to_i))
+      puts "That one is taken \'#{@player}', try again: "
+    end
+  end while(!valid)
+  [input_row.to_i, input_column.to_i]
+end
+
+def valid_move?(input_row, input_column)
+  @board[input_row. -1][input_column. -1].strip.empty?
+end
+
+def update_board(inputs)
+  @board[inputs[0]-1][inputs[1]-1] = @player
+end
+
+def play
+  inputs = get_inputs
+  update_board(inputs)
+  print_board
+end
+
+def switch_player
+  if(@player == PLAYER_ONE)
+    @player = PLAYER_TWO
+  else
+    @player = PLAYER_ONE
+  end
+end
+
+def game_over?
+  @count -= 1
+  @count <= 0
+end
+
+def print_board
+  puts "--+---+---"
+  @board.each do |row|
+    puts row.join(" | ")
+    puts "--+---+---"
+  end
+end
+
+
+initialize_board
+print_board
+begin
+  break if !play
+  switch_player
+end while !game_over?
+puts "GAME OVER" if(game_over?)

b36595c9493b9b12a7d98eeb0a71bd53268b3439
Christian Salazar <calaca89@gmail.com>
2019-11-10 15:43:10 -0500

    Fixing linter errors

--- lib/handlers.rb
+++ lib/handlers.rb
@@ -7,7 +7,6 @@ class Handlers
   def initialize(board = Board.new)
     @moves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     @board = board
-    @position
   end
 
   def winner?(player)

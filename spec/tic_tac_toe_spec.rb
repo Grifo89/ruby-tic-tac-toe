43985e4c7ccee8f2ffc5fc0b5a7ac38ec58613d5
Christian Salazar <calaca89@gmail.com>
2019-11-18 11:20:38 -0500

    testing refactored

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -6,104 +6,77 @@ require_relative '../lib/player'
 
 RSpec.describe Handlers do
   let(:player) { Player.new('X', 'Christian') }
+  let(:player2) { Player.new('O', 'Laura') }
+  let(:newBoard) { Board.new }
+  let(:game) { Handlers.new(newBoard) }
 
   describe '#game_over?' do
     it 'Returns :winner if the first row is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [%w[X X X], [4, 5, 6], [7, 8, 9]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 1)
+      newBoard.set_cell(player, 2)
+      newBoard.set_cell(player, 3)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if the second row is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [[1, 2, 3], %w[X X X], [7, 8, 9]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 4)
+      newBoard.set_cell(player, 5)
+      newBoard.set_cell(player, 6)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if the third row is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [[1, 2, 3], [4, 5, 6], %w[X X X]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 7)
+      newBoard.set_cell(player, 8)
+      newBoard.set_cell(player, 9)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if the first column is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [['X', 2, 3], ['X', 5, 6], ['X', 8, 9]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 1)
+      newBoard.set_cell(player, 4)
+      newBoard.set_cell(player, 7)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if the second column is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [[1, 'X', 3], [4, 'X', 6], [7, 'X', 9]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 2)
+      newBoard.set_cell(player, 5)
+      newBoard.set_cell(player, 8)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if the third column is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [[1, 2, 'X'], [4, 5, 'X'], [7, 8, 'X']]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 3)
+      newBoard.set_cell(player, 6)
+      newBoard.set_cell(player, 9)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if a diagonal is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [['X', 2, 3], [4, 'X', 6], [7, 8, 'X']]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 1)
+      newBoard.set_cell(player, 5)
+      newBoard.set_cell(player, 9)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it 'Returns :winner if the other diagonal is filled with the same symbol' do
-      class Board1 < Board
-        private def default_grid
-          [[1, 2, 'X'], [4, 'X', 6], ['X', 8, 9]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 3)
+      newBoard.set_cell(player, 5)
+      newBoard.set_cell(player, 7)
       expect(game.game_over?(player)).to eql(:winner)
     end
 
     it "Returns :draw if all cells are filled and there's not a winner" do
-      class Board1 < Board
-        private def default_grid
-          [%w[X O X], %w[O X X], %w[O X O]]
-        end
-      end
-      board1 = Board1.new
-      game = Handlers.new(board1)
+      newBoard.set_cell(player, 1)
+      newBoard.set_cell(player2, 2)
+      newBoard.set_cell(player, 3)
+      newBoard.set_cell(player2, 4)
+      newBoard.set_cell(player, 5)
+      newBoard.set_cell(player2, 6)
+      newBoard.set_cell(player, 8)
+      newBoard.set_cell(player2, 7)
+      newBoard.set_cell(player2, 9)
       expect(game.game_over?(player)).to eql(:draw)
     end
   end
